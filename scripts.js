ab8b125b193bd0fb842bc91b788b5b917e892ae7
Artur Quirino <artur.pquirino@gmail.com>
2020-12-14 20:36:17 -0300

    Adicionando produtos e carrinho

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
 console.log("Hello World")
+
+let valorTotal = 0;
+
+const adicionarNoCarrinho = (value) => {
+  valorTotal += value;
+  document.getElementById('valorCarrinho').innerHTML = valorTotal + ',00';
+}
+
+const removerNoCarrinho = (value) => {
+  valorTotal -= value;
+  document.getElementById('valorCarrinho').innerHTML = valorTotal + ',00';
+}
